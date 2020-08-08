da09d00451a7108619aa73fed7fda95b33ae39ba
augu65 <jstegman@uoguelph.ca>
2020-08-08 14:00:45 -0400

    partially done

--- web_scraper.py
+++ web_scraper.py
@@ -2,7 +2,8 @@
 import requests
 from bs4 import BeautifulSoup
 import json
-
+import pycurl
+from io import BytesIO
 
 def get_webpages(webpage) -> dict:
     """
@@ -10,6 +11,24 @@ def get_webpages(webpage) -> dict:
     """
     page = requests.get(webpage)
     soup = BeautifulSoup(page.text, 'html.parser')
+    return soup
+
+
+def curl_stuff(website) -> dict:
+    """
+    uses curl because some websites are mean
+    """
+    b_obj = BytesIO()
+    crl = pycurl.Curl()
+    crl.setopt(crl.URL, website)
+    crl.setopt(crl.WRITEDATA, b_obj)
+    crl.setopt(pycurl.SSL_VERIFYPEER, 0)
+    crl.setopt(pycurl.SSL_VERIFYHOST, 0)
+    crl.perform()
+    crl.close()
+    get_body = b_obj.getvalue()
+    print(get_body.decode('utf8'))
+    soup = BeautifulSoup(get_body.decode('utf8'), 'html.parser')
     return soup
 
 def scrape_canada() -> list:
@@ -36,8 +55,20 @@ def scrape_canada() -> list:
             break
     return data2
 
+
+def scrape_eu()->list:
+    """
+    scrapes visa data from the eu website
+    """
+    data = curl_stuff("https://www.schengenvisainfo.com/etias/#countries-need-etias")
+    data = data.body
+    file =open("test.html","w")
+    file.write(str(data))
+    file.close()
+    return data
+
 def main():
-    data = scrape_canada()
+    data = scrape_eu()
     print(data)
 if __name__ == "__main__":
     main()
