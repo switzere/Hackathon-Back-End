d86bc75d91d667d7bf95c1240ea83750482cbf1e
switzere <switzere@uoguelph.ca>
2020-08-08 13:48:07 -0400

    Created scraped for US

--- web_scraper.py
+++ web_scraper.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 import requests
+import re
 from bs4 import BeautifulSoup
 import json
 
@@ -8,14 +9,34 @@ def get_webpages(webpage) -> dict:
     """
     This function returns the resulting dom for a webpage.
     """
-    page = requests.get(f"http://www.{webpage}")
+    page = requests.get(webpage)
     soup = BeautifulSoup(page.text, 'html.parser')
     return soup
 
+def scrape_US() -> list:
+    data = get_webpages("https://travel.state.gov/content/travel/en/us-visas/tourism-visit/visa-waiver-program.html")
+    us_li = []
+    stripped_li = []
+    li = data.find_all("ul")
+
+    us_li = li[16].text + li[17].text + li[18].text
+    us_li = us_li.split("\n")
+    us_li = list(filter(None, us_li))
+
+    for new_ul in us_li:
+        stripped_li.append(new_ul.replace('*', ''))
+        #print(''.join(filter(str.isalnum, new_ul)))
+    #us_li = [re.sub("*","",x) for x in us_li]
+
+    print(stripped_li)
+
+    return stripped_li
 
 def main():
-    data = get_webpages("google.com")
-    print(data)
+
+    US_list = scrape_US()
+    print(US_list)
+
 
 if __name__ == "__main__":
     main()
