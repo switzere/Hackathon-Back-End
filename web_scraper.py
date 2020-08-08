83c1b6a3647a8898f5bff987c73ce4068e6dceed
augu65 <jstegman@uoguelph.ca>
2020-08-08 13:27:08 -0400

    scraped canada webste

--- web_scraper.py
+++ web_scraper.py
@@ -8,14 +8,36 @@ def get_webpages(webpage) -> dict:
     """
     This function returns the resulting dom for a webpage.
     """
-    page = requests.get(f"http://www.{webpage}")
+    page = requests.get(webpage)
     soup = BeautifulSoup(page.text, 'html.parser')
     return soup
 
+def scrape_canada() -> list:
+    """
+    scrapes the data froms the canadian immigration website
+    """
+    ctr = 0
+    data2 = []
+    found = False
+    data = get_webpages(
+        "https://www.canada.ca/en/immigration-refugees-citizenship/services\
+        /visit-canada/entry-requirements-country.html#visaExempt"
+        )
+    data = data.body.find_all("ul")
+    for line in data:
+        print(line)
+        for l in line.findChildren("li"):
+            if ctr == 3:
+                data2.append(l.text)
+                found = True
+            if ">eTA exemptions</a" in str(l):
+                ctr += 1
+        if found is True:
+            break
+    return data2
 
 def main():
-    data = get_webpages("google.com")
+    data = scrape_canada()
     print(data)
-
 if __name__ == "__main__":
     main()
